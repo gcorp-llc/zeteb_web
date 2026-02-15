e054819421c6a878b6ea5c2567afb399dfca00ec
HOSSEIN EFTEKHARRAD <51985898+gcorp-llc@users.noreply.github.com>
2026-02-15 13:43:48 -0600

    Remove Glass Card Style Pages

--- components/page-container.tsx
+++ components/page-container.tsx
@@ -8,7 +8,7 @@ export const PageContainer = ({
   return (
     <div className="max-w-7xl mx-auto px-6 pt-24 pb-32 animate-ios-in">
       <h1 className="text-4xl font-black mb-8">{title}</h1>
-      <div className="glass-card min-h-[400px]">
+      <div className="min-h-[400px]">
         {children}
       </div>
     </div>
